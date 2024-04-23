5bc5e9b79a803ddca322a9e529bf6aedf4617f7f
Ananda Langhans <ananda.defreitaslangh01@universitadipavia.it>
2024-04-23 17:44:08 +0200

    Creazione dei modelli (incompletta)

---
+++ src/file.js
@@ -0,0 +1,8 @@
+class file {
+  constructor(url, title, folderId) {
+    this._url = url; // indirizzo per prelevare il file
+    this._fileId = id; //identificativo file
+    this._title = title; // [obbligatorio] titolo
+    this._folderId = folderId; //[opzionale] id della cartella dove verrà caricato il file
+  }
+}
---
+++ src/folder.js
@@ -0,0 +1,8 @@
+class folder {
+  constructor(istitution, course, argument) {
+    this.folderId = id; // identificativo della cartella
+    this.istitution = istitution; //categoria della cartella (università)
+    this.course = course; // categoria della cartella (corso)
+    this.argument = argument; //categoria della cartella (argomento)
+  }
+}
